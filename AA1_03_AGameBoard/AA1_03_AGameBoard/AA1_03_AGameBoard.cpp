284a3d0972b03f57b226d1e12120aecb9f94815c
Edgar Novas <edgar.novas@enti.cat>
2024-03-20 00:23:08 +0100

    existCoin done

--- AA1_03_AGameBoard/AA1_03_AGameBoard/AA1_03_AGameBoard.cpp
+++ AA1_03_AGameBoard/AA1_03_AGameBoard/AA1_03_AGameBoard.cpp
@@ -14,6 +14,7 @@ int main()
     Board** board = new Board*[NUM_ROWS];
     Player player;
     player.money = 0;
+    
 
 
     for (int i = 0; i < NUM_ROWS; i++) {
@@ -31,20 +32,20 @@ int main()
     //DELETE CODE
     //When we end we delete the pointers of the heap
 
-    for (int i = 0; i < NUM_ROWS; i++) {
-        delete[] board[i];
-    }
+for (int i = 0; i < NUM_ROWS; i++) {
+    delete[] board[i];
+}
 
-    //Delete form ram
-    delete[] board;
+//Delete form ram
+delete[] board;
 
-    board = nullptr;
+board = nullptr;
 
 
 }
 
 
-void initializeBoard(int colums, int rows, Board **board,Player player) {
+void initializeBoard(int colums, int rows, Board** board, Player& player) {
 
     int totalTiles = colums * rows;
 
@@ -58,7 +59,7 @@ void initializeBoard(int colums, int rows, Board **board,Player player) {
 
     int randomX = rand() % rows;
     int randomY = rand() % colums;
-   
+
     player.x = randomX;
     player.y = randomY;
 
@@ -67,7 +68,7 @@ void initializeBoard(int colums, int rows, Board **board,Player player) {
             //std::cout <<board[i][j];
             if (i == randomX && j == randomY) {
                 board[i][j].jugador = true;
-                
+
             }
             else
             {
@@ -77,11 +78,11 @@ void initializeBoard(int colums, int rows, Board **board,Player player) {
                     //choose between both objetcs with a random
                     int fill = rand() % 2;
 
-                    if (fill == 0 && stones >= 0) {          
+                    if (fill == 0 && stones > 0) {
                         board[i][j].piedras = true;
                         stones--;
                     }
-                    else if(fill == 1 && coins >= 0)
+                    else if (fill == 1 && coins > 0)
                     {
                         board[i][j].monedas = true;
                         coins--;
@@ -95,35 +96,94 @@ void initializeBoard(int colums, int rows, Board **board,Player player) {
 
 
 
-bool checkMovement(Player pos, Movement move) {
+bool checkMovement(Player& pos, Movement move) {
 
     switch (move)
     {
 
     case Movement::UP:
-        if (pos.y - 1 >= 0) {
-            //Can go up
+        if (pos.x > 0 ) {
+            pos.lastMove = move;// Si el movimiento hacia arriba no lleva al jugador fuera del tablero
+            return true;
         }
-        
+            
         break;
     case Movement::DOWN:
-        if (pos.y + 1 <= NUM_COLUMNS) {
-
+        if (pos.x < NUM_ROWS - 1) // Si el movimiento hacia abajo no lleva al jugador fuera del tablero
+        {
+            pos.lastMove = move;// Si el movimiento hacia arriba no lleva al jugador fuera del tablero
+            return true;
         }
+            
         break;
     case Movement::LEFT:
+        if (pos.y > 0) // Si el movimiento hacia la izquierda no lleva al jugador fuera del tablero
+        {
+            pos.lastMove = move;// Si el movimiento hacia arriba no lleva al jugador fuera del tablero
+            return true;
+        }
         break;
     case Movement::RIGHT:
+        if (pos.y < NUM_COLUMNS - 1) // Si el movimiento hacia la derecha no lleva al jugador fuera del tablero
+        {
+            pos.lastMove = move;// Si el movimiento hacia arriba no lleva al jugador fuera del tablero
+            return true;
+        }
         break;
     default:
+        return false;
         break;
     }
-
-    return false;
-
-        
     
 
+    return false;
+}
+
+void addScore(Player& player) {
+    player.score++;
+}
+
+void setPos(Player& player, Board** board) {
+    //En que i and j esta que es x e y
+    if (player.lastMove == Movement::UP) {
+        if (board[player.x][player.y - 1].piedras == false) {
+            board[player.x][player.y].jugador = false;
+            player.y -= 1;
+            board[player.x][player.y].jugador = true;
+            
+        }
+    }
+    else if (player.lastMove == Movement::DOWN) {
+        if (board[player.x][player.y + 1].piedras == false) {
+            board[player.x][player.y].jugador = false;
+            player.y += 1;
+            board[player.x][player.y].jugador = true;
+
+        }
+    }
+    else if (player.lastMove == Movement::LEFT) {
+        if (board[player.x -1][player.y].piedras == false) {
+            board[player.x][player.y].jugador = false;
+            player.x -= 1;
+            board[player.x][player.y].jugador = true;
+
+        }
+    }
+    else if (player.lastMove == Movement::RIGHT) {
+        if (board[player.x +1][player.y].piedras == false) {
+            board[player.x][player.y].jugador = false;
+            player.y += 1;
+            board[player.x][player.y].jugador = true;
+
+        }
+    }
 }
 
 
+bool existCoins(Board** board, Player& player) {
+    if (board[player.x][player.y].monedas == true) {
+        return true;
+    }
+    
+    return false;
+}
--- AA1_03_AGameBoard/AA1_03_AGameBoard/GameBoard.h
+++ AA1_03_AGameBoard/AA1_03_AGameBoard/GameBoard.h
@@ -20,11 +20,17 @@ struct Player
     int x;
     int y;
     int money;
+    int score;
+    Movement lastMove;
 };
 
-void initializeBoard(int colums, int rows, Board** board, Player player);
+void initializeBoard(int colums, int rows, Board** board, Player& player);
 
 
-bool checkMovement(Player pos, Movement move);
+bool checkMovement(Player& pos, Movement move);
 
+void addScore(Player& player);
 
+void setPos(Player& player, Board** board);
+
+bool existCoins(Board** board, Player& player);
